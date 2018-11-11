413563d5d29fe0e640aeb1a6b30cbce9eb128d3d
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2018-11-11 11:00:57 -0500

    Added audio file layout

---
+++ audio.js
@@ -0,0 +1,22 @@
+const bot = require("./bot.js");
+const keywords = require("./keywords.json")
+
+/*
+	Handles voice channel related processing
+*/
+
+exports.isInCall = function(msg) {
+	console.log("isInCall method not completed.");
+	return false;
+}
+
+exports.checkKeyword = function(msgContent) {
+	console.log("checkKeyword method not completed.");
+	return false;
+}
+
+exports.playKeyword = function(msg) {
+	console.log("playKeyword method not completed.");
+	return;
+}
+
--- bot.js
+++ bot.js
@@ -1,13 +1,26 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
+const Discord = require("discord.js");
+const config = require("./config.json");
+const audio = require("./audio.js");
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	console.log('\n%s\x1b[32m%s\x1b[0m\n', 'Logged in as ', client.user.tag); 
+
+	console.log('\n%s\x1b[32m%s\x1b[0m\n', 'Logged in as: ', client.user.tag); 
+
+	client.user.setPresence({ 
+		game: { name: "Under maintenance" }, 
+		status: "online" })
+	.then(console.log)
+	.catch(console.error);
+
 });
 
 client.on('message', msg => {
+
+	//Handles playback of keyword-triggered audio clips in voice channels
+	if(audio.isInCall(msg) && audio.checkKeyword(msg.content))
+		audio.playKeyword(msg);
 
 	if (msg.content.length != 0 && msg.content[0] == '!')
 		msg.reply('Dannybot is currently unavailable.');
