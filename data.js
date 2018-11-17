36df110267b134162cafe8e528e6c82c18676e75
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2018-11-17 12:56:20 -0500

    Added placeholder functions

--- data.js
+++ data.js
@@ -1,3 +1,13 @@
 /*
 	Handles file input/output for storage of server and user information.
 */
+
+exports.makeGuildFile = function(guild) {
+	console.log("Error: makeGuildFile method not completed.");
+	return;
+}
+
+exports.deleteGuildFile = function(guild) {
+	console.log("Error: deleteGuildFile method not completed.");
+	return;
+}
