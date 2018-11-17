0e6d5b952413ddff70057f2d418057759af35da8
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2018-11-17 12:56:42 -0500

    Added console message formatting

--- audio.js
+++ audio.js
@@ -2,18 +2,36 @@
 	Handles voice channel related processing
 */
 
-exports.isInCall = function(msg) {
-	console.log("isInCall method not completed.");
+var red = "\x1b[31m", bright = "\x1b[1m", reset = "\x1b[0m";
+
+exports.isInGroupDMCall = function(msg) {
+	console.log(bright + red + "%s" + reset + "%s\n", "Error: ", "isInGroupDMCall method not completed.");
 	return false;
 }
 
-exports.checkKeyword = function(msgContent) {
-	console.log("checkKeyword method not completed.");
+exports.isInGuildCall = function(msg) {
+	console.log(bright + red + "%s" + reset + "%s\n", "Error: ", "isInGuildCall method not completed.");
 	return false;
 }
 
-exports.playKeyword = function(msg) {
-	console.log("playKeyword method not completed.");
+exports.checkGroupDMKeyword = function(msgContent) {
+	console.log(bright + red + "%s" + reset + "%s\n", "Error: ", "checkGroupDMKeyword method not completed.");
+	return false;
+}
+
+exports.checkGuildKeyword = function(msgContent) {
+	console.log(bright + red + "%s" + reset + "%s\n", "Error: ", "checkGuildKeyword method not completed.");
+	return false;
+}
+
+exports.playGroupDMKeyword = function(msg) {
+	console.log(bright + red + "%s" + reset + "%s\n", "Error: ", "playGroupDMKeyword method not completed.");
 	return;
 }
+
+exports.playGuildKeyword = function(msg) {
+	console.log(bright + red + "%s" + reset + "%s\n", "Error: ", "playGuildKeyword method not completed.");
+	return;
+}
+
 
