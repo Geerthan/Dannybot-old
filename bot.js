6ff17e32b83b806ab784cf48f7ce31744683d451
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2018-11-17 12:55:39 -0500

    Adding in template code

--- bot.js
+++ bot.js
@@ -7,42 +7,56 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 
-	console.log('\n%s\x1b[32m%s\x1b[0m\n', 'Logged in as: ', client.user.tag); 
+	console.log("\n\x1b[32m%s\x1b[0m%s\n", "Logged in as: ", client.user.tag); 
 
 	client.user.setPresence({ 
 		game: { name: config.status }, 
 		status: "online" })
-	.then(console.log)
 	.catch(console.error);
 
 });
 
-client.on('guildCreate', guild => {
+client.on("guildCreate", guild => {
 	data.makeGuildFile(guild);
 });
 
-client.on('guildDelete', guild => {
+client.on("guildDelete", guild => {
 	data.deleteGuildFile(guild);
 });
 
-client.on('message', msg => {
+client.on("message", msg => {
 
 	// Group Chats outside of Guilds
 	if(msg.channel instanceof Discord.GroupDMChannel) {
 		
+		if(!data.hasGroupDMInfo(msg.channel))
+			data.makeGroupDMFile(msg.channel);
+
+		// Handles playback of keyword-triggered audio clips in voice channels
+		if(audio.isInGroupDMCall(msg) && audio.checkGroupDMKeyword(msg)) {
+			msg.reply("Dannybot is currently unavailable.");
+			//audio.playGroupDMKeyword(msg);
+		}
+
+		// Handles custom commands
+		if (msg.content.length != 0 && msg.content[0] == '!')
+			msg.reply("Dannybot is currently unavailable.");
+
 	}
 
 	// Guild Channel
 	else if(msg.channel instanceof Discord.TextChannel) {
 
 		// Handles playback of keyword-triggered audio clips in voice channels
-		if(audio.isInCall(msg) && audio.checkKeyword(msg))
-			audio.playKeyword(msg);
-
-		if (msg.content.length != 0 && msg.content[0] == '!')
-			msg.reply('Dannybot is currently unavailable.');
-
+		if(audio.isInGuildCall(msg) && audio.checkGuildKeyword(msg)) {
+			msg.reply("Dannybot is currently unavailable.");
+			//audio.playGuildKeyword(msg);
 		}
+
+		// Handles custom commands
+		if (msg.content.length != 0 && msg.content[0] == '!')
+			msg.reply("Dannybot is currently unavailable.");
+	}
 
 	// Direct Messages
 	else { 
